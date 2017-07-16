61036ff9e3d3279afb19a434ddfb8702c3707120
tranki1 <kimhieutran100@gmail.com>
2017-07-16 17:41:42 +0300

    basic express

---
+++ node-web-server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+let app = express();
+
+app.get('/', (req, res) => {
+  // res.send('<h1>hello express</h1>');
+  res.send({
+    name: 'kim',
+    like: ['food', 'drinks']
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.send('about page');
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'page not found'
+  });
+});
+
+app.listen(3000);
