474ade7ab43c299f3ef7da5de8b69d67b433b980
tranki1 <kimhieutran100@gmail.com>
2017-07-16 18:06:11 +0300

    redering template(handlebars) with data

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,19 +1,29 @@
 const express = require('express');
+const hbs = require('hbs');
 
 let app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   // res.send('<h1>hello express</h1>');
-  res.send({
+  // res.send({
+  //   name: 'kim',
+  //   like: ['food', 'drinks']
+  // });
+  res.render('home.hbs', {
+    pageTitle: 'Home',
     name: 'kim',
-    like: ['food', 'drinks']
+    currentYear: new Date().getFullYear()
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('about page');
+  res.render('about.hbs', {
+    pageTitle: 'about',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get('/bad', (req, res) => {
