bd5abf523040f01cb71d477d60949fd6bbf3dbe6
tranki1 <kimhieutran100@gmail.com>
2017-07-18 09:47:43 +0300

    using assertion library

--- node-tests/utils/utils.js
+++ node-tests/utils/utils.js
@@ -1,2 +1,8 @@
 module.exports.add = (a, b) => a + b;
 module.exports.square = a => a * a;
+module.exports.setName = (user, fullname) => {
+  let name = fullname.split(' ');
+  user.firstname = name[0];
+  user.lastname = name[1];
+  return user;
+};
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,15 +1,39 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('should add two numbers', () => {
-  var res = utils.add(44, 11);
-  if (res !== 55) {
-    throw new Error(`expect 55 but got ${res}`);
-  }
+  let res = utils.add(44, 11);
+  // if (res !== 55) {
+  //   throw new Error(`expect 55 but got ${res}`);
+  // }
+
+  expect(res)
+    .toBe(55)
+    .toBeA('number');
 });
 
 it('should square', () => {
-  var res = utils.square(3);
-  if (res !== 9) {
-    throw new Error(`expect 9 but got ${res}`);
-  }
+  let res = utils.square(3);
+  // if (res !== 9) {
+  //   throw new Error(`expect 9 but got ${res}`);
+  // }
+
+  expect(res).toBe(9);
+});
+
+it('should set firstname and lastname', () => {
+  let user = { location: 'vietnam', age: 27 };
+  let res = utils.setName(user, 'Kim Tran');
+  expect(res).toEqual(user);
+  expect(res).toInclude({
+    firstname: 'Kim',
+    lastname: 'Tran'
+  });
+});
+
+it('should expect some value', () => {
+  // expect(12).toNotBe(12);
+  // expect({ name: 'Kim' }).toEqual({ name: 'Kim' });
+  expect([2, 3, 4]).toExclude(1);
+  expect([2, 3, 4]).toInclude(2);
 });
