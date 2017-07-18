c53d4ff2e4d67a3c3c18aad643410c855a56d2c9
tranki1 <kimhieutran100@gmail.com>
2017-07-18 16:00:48 +0300

    fetch data

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,7 +1,9 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 MongoClient.connect(
   'mongodb://localhost:27017/TodoApp',
+  { useNewUrlParser: true },
   (err, client) => {
     if (err) {
       console.log('Unable to connect to MongoDB server');
@@ -24,20 +26,22 @@ MongoClient.connect(
     //   }
     // );
 
-    db.collection('Users').insertOne(
-      {
-        name: 'Kim',
-        age: 27,
-        location: 'Finland'
-      },
-      (err, result) => {
-        if (err) {
-          console.log('Unable to insert User', err);
-        }
+    // db.collection('Users').insertOne(
+    //   {
+    //     name: 'Kim',
+    //     age: 27,
+    //     location: 'Finland'
+    //   },
+    //   (err, result) => {
+    //     if (err) {
+    //       console.log('Unable to insert User', err);
+    //     }
 
-        console.log(JSON.stringify(result.ops, undefined, 2));
-      }
-    );
+    //     console.log(result.ops[0]._id.getTimestamp());
+
+    //     // console.log(JSON.stringify(result.ops, undefined, 2));
+    //   }
+    // );
 
     client.close();
   }
