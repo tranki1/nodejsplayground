cd8eb8e9da9d747482e484a36e1db5b8fdb276fd
tranki1 <kimhieutran100@gmail.com>
2017-07-19 09:26:20 +0300

    Validators, Types, and Defaults

--- server/server.js
+++ server/server.js
@@ -8,18 +8,23 @@ mongoose.connect(
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
 var newTodo = new Todo({
-  text: 'Cook dinner'
+  text: '    Edit this video    '
 });
 
 newTodo.save().then(
@@ -31,15 +36,37 @@ newTodo.save().then(
   }
 );
 
-var otherTodo = new Todo({
-  text: 'Eating sugar',
-  completed: true,
-  completeAt: 123
+// var otherTodo = new Todo({
+//   text: 'Eating sugar',
+//   completed: true,
+//   completeAt: 123
+// });
+
+// otherTodo.save().then(
+//   doc => {
+//     console.log(JSON.stringify(doc, undefined, 2));
+//   },
+//   err => console.log('Unable to save other todo')
+// );
+
+var User = mongoose.model('User', {
+  email: {
+    required: true,
+    trim: true,
+    type: String,
+    minlength: 1
+  }
 });
 
-otherTodo.save().then(
+var newUser = new User({
+  email: 'kimtran@gmail.com'
+});
+
+newUser.save().then(
   doc => {
     console.log(JSON.stringify(doc, undefined, 2));
   },
-  err => console.log('Unable to save other todo')
+  e => {
+    console.log(e);
+  }
 );
