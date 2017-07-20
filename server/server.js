de9e0c5423d2929b53e2d6ec720cf24568dbf0fb
tranki1 <kimhieutran100@gmail.com>
2017-07-20 22:56:41 +0300

    GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -33,6 +34,26 @@ app.get(
     res.status(400).send(error);
   }
 );
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        res.status(404).send();
+      }
+
+      res.send({ todo });
+    })
+    .catch(err => {
+      res.status(404).send();
+    });
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
