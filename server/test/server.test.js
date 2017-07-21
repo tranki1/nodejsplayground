7b6bb3bb3bf5d2cd5792f5f6446b58db4150193a
tranki1 <kimhieutran100@gmail.com>
2017-07-21 11:33:32 +0300

    Testing DELETE /todos/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -107,3 +107,44 @@ describe('GET /todos/:id', () => {
       .end(done);
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', done => {
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexId)
+          .then(todo => {
+            expect(todo).toBe(null);
+            done();
+          })
+          .catch(err => done(err));
+      });
+  });
+
+  it('should return 404 if todo not found', done => {
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+      .delete(`/todo/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non object id', done => {
+    request(app)
+      .get(`/todos/1223413`)
+      .expect(404)
+      .end(done);
+  });
+});
